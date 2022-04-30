24dafec50dcb4c329ec3c9cb61f559a7e27d8a50
Claudio Mezzasalma <claudio.mezzasalma@cloudacademy.com>
2022-04-30 23:39:38 +0200

    add stm32l432 eeprom emulation

---
+++ keyboards/keychron/q2/rev_0111/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7B, 0x43, 0x76, 0x0B, 0x4B, 0x34, 0xA3, 0x78}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
